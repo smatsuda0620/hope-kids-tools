d583d1667b6481591c6490bb58980930bd3b0af7
Shuji Matsuda <shuji.mazdawso@gmail.com>
2019-03-29 21:44:24 +0900

    Organize dependencies of package.json

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,13 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack/webpack.base.config.js');
 
 const config = merge(baseConfig, {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
 });
 
 module.exports = config;
